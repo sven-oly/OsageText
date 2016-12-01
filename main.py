652cf3597fa939ce0bc2f12254672aa563f86d8a
sven-oly <cwcornelius@gmail.com>
2016-11-30 22:12:53 -0800

    Adding data for AppEngine project.

---
+++ main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import json
+import logging
+import os
+import urllib
+import webapp2
+
+from google.appengine.ext.webapp import template
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+      template_values = {'fontFamilies': OsageFonts,
+      }
+      path = os.path.join(os.path.dirname(__file__), 'osage.html')
+      self.response.out.write(template.render(path, template_values))
+      
+# The Unicode fonts from Osage Nation.
+OsageFonts = ['Pawhuska', 'Wynona', 'Avant', 'Barnsdall', 'Nelagoney', 
+  'Prue', 'Wazhezhe']
+  
+class OsageFontTest(webapp2.RequestHandler):
+  def get(self):
+    template_values = {'fontFamilies': OsageFonts,
+    }
+    
+    path = os.path.join(os.path.dirname(__file__), 'osageFonts.html')
+    self.response.out.write(template.render(path, template_values))
+    
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/OsageFontTest/', OsageFontTest), 
+	], debug=True)
+    
