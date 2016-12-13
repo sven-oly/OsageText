8b53d793fe0df035a83923f95473869eab55a46a
sven-oly <cwcornelius@gmail.com>
2016-12-12 23:30:56 -0800

    Fix conversion errors with vowels and "T". Add "tools" to show hex values and toggle details. Add option to omit Old Osage dot.

--- js/osageConverter.js
+++ js/osageConverter.js
@@ -2,6 +2,7 @@ var macron = "\u0304";
 var combiningDotAboveRight = "\u0358";
 
 var osage_private_use_map = {
+  '\uf020': ' ',
   '\uf021': '!',
   '\uf022': [String.fromCodePoint(0x104c7), String.fromCodePoint(0x104ef)],
   '\uf023': '#',
@@ -36,11 +37,15 @@ var osage_private_use_map = {
   '\uf040': '@',
   '\uf041\uf041': [String.fromCodePoint(0x104b0)+macron, String.fromCodePoint(0x104d8)+macron],
   '\uf041': [String.fromCodePoint(0x104b0), String.fromCodePoint(0x104d8)],
+  '\uf041^': [String.fromCodePoint(0x104b0)+combiningDotAboveRight,
+     String.fromCodePoint(0x104d8)+combiningDotAboveRight],
   '\uf042': [String.fromCodePoint(0x104b4), String.fromCodePoint(0x104dc)],
   '\uf043': [String.fromCodePoint(0x104b5), String.fromCodePoint(0x104dd)],
   '\uf044': [String.fromCodePoint(0x104c8), String.fromCodePoint(0x104f0)],
   '\uf045\uf045': [String.fromCodePoint(0x104b7)+macron, String.fromCodePoint(0x104de)+macron],
   '\uf045': [String.fromCodePoint(0x104b7), String.fromCodePoint(0x104de)],
+  '\uf045^': [String.fromCodePoint(0x104b7)+combiningDotAboveRight,
+     String.fromCodePoint(0x104de)+combiningDotAboveRight],
   '\uf048': [String.fromCodePoint(0x104b9), String.fromCodePoint(0x104e1)],
   // The eh-consonants
   '\uf048\uf043': [String.fromCodePoint(0x104b6), String.fromCodePoint(0x104de)],
@@ -57,16 +62,22 @@ var osage_private_use_map = {
   '\uf04e': [String.fromCodePoint(0x104c1), String.fromCodePoint(0x104e9)],
   '\uf04f\uf04f': [String.fromCodePoint(0x104c2)+macron, String.fromCodePoint(0x104ea)+macron],
   '\uf04f': [String.fromCodePoint(0x104c2), String.fromCodePoint(0x104ea)],
+  '\uf04f^': [String.fromCodePoint(0x104c2)+combiningDotAboveRight,
+    String.fromCodePoint(0x104ea)+combiningDotAboveRight],
   '\uf050': [String.fromCodePoint(0x104c4), String.fromCodePoint(0x104ec)],
   '\uf053': [String.fromCodePoint(0x104c6), String.fromCodePoint(0x104ee)],
   '\uf054': [String.fromCodePoint(0x104cd), String.fromCodePoint(0x104c6)],
   '\uf055\uf055': [String.fromCodePoint(0x104ce)+macron, String.fromCodePoint(0x104f6)+macron],
   '\uf055': [String.fromCodePoint(0x104ce), String.fromCodePoint(0x104f6)],
+  '\uf055^': [String.fromCodePoint(0x104ce)+combiningDotAboveRight,
+    String.fromCodePoint(0x104f6)+combiningDotAboveRight],
   '\uf056': [String.fromCodePoint(0x104c7), String.fromCodePoint(0x104ef)],
   '\uf057': [String.fromCodePoint(0x104cf), String.fromCodePoint(0x104f7)],
   '\uf058': [String.fromCodePoint(0x104d0), String.fromCodePoint(0x104f8)],
   '\uf059\uf059': [String.fromCodePoint(0x104bb)+macron, String.fromCodePoint(0x104e3)+macron],
   '\uf059': [String.fromCodePoint(0x104bb), String.fromCodePoint(0x104e3)],
+  '\uf059^': [String.fromCodePoint(0x104bb)+combiningDotAboveRight,
+    String.fromCodePoint(0x104e3)+combiningDotAboveRight],
   '\uf05a': [String.fromCodePoint(0x104d2), String.fromCodePoint(0x104fa)],  // ??
   '\uf05b': [String.fromCodePoint(0x104d3), String.fromCodePoint(0x104fb)],  // ??
   '\uf05c': [' ', ' '],  // Character is no longer used.
@@ -86,20 +97,22 @@ var osage_private_use_map = {
 
 // Handles upper case, too.
 var osage_latin_to_unicode_map = {
-  'a': [String.fromCodePoint(0x104d8), '\uf041'],
   // 'a': [String.fromCodePoint(0x104d8), '\uf041'],
-  'a': [String.fromCodePoint(0x104b0)+macron, '\uf041\uf041'], // Macron
+  'a': [String.fromCodePoint(0x104b2), '\uf061'],
   'aa': [String.fromCodePoint(0x104d8)+macron, '\uf041\uf041'], // Macron
   'a\'': [String.fromCodePoint(0x104d9), '\uf049'],
   'an': [String.fromCodePoint(0x104da), '\uf061'],
   'ah': [String.fromCodePoint(0x104db), '\uf04a'],
+  'a^': [String.fromCodePoint(0x104d8)+combiningDotAboveRight, '\uf04a^'],
   'b':  [String.fromCodePoint(0x104dc), '\uf042'],
   'br': [String.fromCodePoint(0x104dc), '\uf042'],
   'hc': [String.fromCodePoint(0x104de), '\uf043'],
   'c':  [String.fromCodePoint(0x104dd), '\uf043'],
   'ch': [String.fromCodePoint(0x104de), '\uf043'],
   'd':  [String.fromCodePoint(0x104f0), '\uf044'],
-  'e':  [String.fromCodePoint(0x104df), '\uf045'],
+  // 'e':  [String.fromCodePoint(0x104df), '\uf045'],
+  'e':  [String.fromCodePoint(0x104b8), '\uf065'],
+  'e^':  [String.fromCodePoint(0x104df)+combiningDotAboveRight, '\uf065^'],
   'ee': [String.fromCodePoint(0x104df)+macron, '\uf045\uf045'], // Macron
   'en': [String.fromCodePoint(0x104e0), '\uf065'],
   'g':  [String.fromCodePoint(0x104f9), '\uf059'],
@@ -114,29 +127,33 @@ var osage_latin_to_unicode_map = {
   'l':  [String.fromCodePoint(0x104e7), '\uf04c'],
   'm':  [String.fromCodePoint(0x104f8), '\uf04d'],
   'n':  [String.fromCodePoint(0x104e9), '\uf04e'],
-//  'o':  [String.fromCodePoint(0x104ea), '\uf04f'],
-  'o': [String.fromCodePoint(0x104c2)+macron, '\uf04f\uf04f'], // Macron
+  // 'o':  [String.fromCodePoint(0x104ea), '\uf04f'],
+  'o': [String.fromCodePoint(0x104c3), '\uf06f'],
+  'o^': [String.fromCodePoint(0x104ea)+combiningDotAboveRight, '\uf06f^'],
   'oo': [String.fromCodePoint(0x104ea)+macron, '\uf04f\uf04f'], // Macron
   'on': [String.fromCodePoint(0x104eb), '\uf06f'],
   'p':  [String.fromCodePoint(0x104ec), '\uf050'],
   'hp': [String.fromCodePoint(0x104ed), '\uf048\uf04b'],
   's':  [String.fromCodePoint(0x104ee), '\uf053'],
   'sh': [String.fromCodePoint(0x104ef), '\uf022'],
-  't':  [String.fromCodePoint(0x104f0), '\uf044'],
+  't':  [String.fromCodePoint(0x104f5), '\uf054'],
   'ht': [String.fromCodePoint(0x104f1), '\uf048\uf04b'],
   'ts': [String.fromCodePoint(0x104f2), '\uf05d'],
   'hts': [String.fromCodePoint(0x104f3), '\uf054'],
   'tsh': [String.fromCodePoint(0x104f4), '\uf054'],
   'u':  [String.fromCodePoint(0x104f6), '\uf055'],
+  'u^': [String.fromCodePoint(0x104f6)+combiningDotAboveRight, '\uf055^'],
   'uu': [String.fromCodePoint(0x104f6)+macron, '\uf055\uf055'], // Macron
   'v':  [String.fromCodePoint(0x104ef), '\uf056'],
   'w':  [String.fromCodePoint(0x104f7), '\uf057'],
   'x':  [String.fromCodePoint(0x104f8), '\uf058'],
   'y':  [String.fromCodePoint(0x104e3), '\uf05a'],
+  'y^':  [String.fromCodePoint(0x104e3)+combiningDotAboveRight, '\uf05a^'],
   'z':  [String.fromCodePoint(0x104fa), '\uf05a'],
   'zh': [String.fromCodePoint(0x104fb), '\uf05b'],
   // Upper case input.
   'A': [String.fromCodePoint(0x104b0), '\uf041'],
+  'A^': [String.fromCodePoint(0x104b0)+combiningDotAboveRight, '\uf041^'],
   'Aa': [String.fromCodePoint(0x104b0)+macron, '\uf041\uf041'], // Macron
   'AA': [String.fromCodePoint(0x104b0)+macron, '\uf041\uf041'], // Macron
   'A\'': [String.fromCodePoint(0x104b1), '\uf049'],
@@ -155,6 +172,7 @@ var osage_latin_to_unicode_map = {
   'CH': [String.fromCodePoint(0x104b6), '\uf043'],
   'D':  [String.fromCodePoint(0x104c8), '\uf044'],
   'E':  [String.fromCodePoint(0x104b7), '\uf045'],
+  'E^':  [String.fromCodePoint(0x104b7)+combiningDotAboveRight, '\uf045^'],
   'Ee': [String.fromCodePoint(0x104b7)+macron, '\uf045\uf045'], // Macron
   'EE': [String.fromCodePoint(0x104b7)+macron, '\uf045\uf045'], // Macron
   'En': [String.fromCodePoint(0x104b8), '\uf065'],
@@ -176,6 +194,7 @@ var osage_latin_to_unicode_map = {
   'M':  [String.fromCodePoint(0x104c0), '\uf04d'],
   'N':  [String.fromCodePoint(0x104c1), '\uf04e'],
   'O':  [String.fromCodePoint(0x104c2), '\uf04f'],
+  'O^':  [String.fromCodePoint(0x104c2)+combiningDotAboveRight, '\uf04f^'],
   'Oo': [String.fromCodePoint(0x104c2)+macron, '\uf04f\uf04f'], // Macron
   'OO': [String.fromCodePoint(0x104c2)+macron, '\uf04f\uf04f'], // Macron
   'On': [String.fromCodePoint(0x104c3), '\uf06f'],
@@ -186,7 +205,7 @@ var osage_latin_to_unicode_map = {
   'S':  [String.fromCodePoint(0x104c6), '\uf053'],
   'Sh': [String.fromCodePoint(0x104c7), '\uf022'],
   'SH': [String.fromCodePoint(0x104c7), '\uf022'],
-  'T':  [String.fromCodePoint(0x104cd), '\uf044'],
+  'T':  [String.fromCodePoint(0x104cd), '\uf054'],
 //  'T':  [String.fromCodePoint(0x104c8), '\uf044'],
   'Ht': [String.fromCodePoint(0x104c9), '\uf048\uf04b'],
   'HT': [String.fromCodePoint(0x104c9), '\uf048\uf04b'],
@@ -199,12 +218,14 @@ var osage_latin_to_unicode_map = {
   'TSh': [String.fromCodePoint(0x104cc), '\uf054'],
   'TSH': [String.fromCodePoint(0x104cc), '\uf054'],
   'U':  [String.fromCodePoint(0x104ce), '\uf055'],
+  'U^':  [String.fromCodePoint(0x104ce)+combiningDotAboveRight, '\uf055^'],
   'Uu': [String.fromCodePoint(0x104ce)+macron, '\uf055\uf055'], // Macron
   'UU': [String.fromCodePoint(0x104ce)+macron, '\uf055\uf055'], // Macron
   'V':  [String.fromCodePoint(0x104c7), '\uf056'],
   'W':  [String.fromCodePoint(0x104cf), '\uf057'],
   'X':  [String.fromCodePoint(0x104d0), '\uf058'],
   'Y':  [String.fromCodePoint(0x104bb), '\uf05a'],
+  'Y^':  [String.fromCodePoint(0x104bb)+combiningDotAboveRight, '\uf05a^'],
   'Z':  [String.fromCodePoint(0x104d2), '\uf05a'],
   'Zh': [String.fromCodePoint(0x104d3), '\uf05b'],  
   'ZH': [String.fromCodePoint(0x104d3), '\uf05b'],  
@@ -226,12 +247,14 @@ var full_map = osage_private_use_map;
 var minOsageU = String.fromCodePoint(0x104b0);
 var maxOsageU = String.fromCodePoint(0x104d8);
 var lowerCaseOffset = 0x28;
+var oldOsageDot = '\uf02e';
 
 // Converts from old Osage codepoints to Unicode Standard.
 // Converts to lower case if the flag is set.
 // Converts Latin characters if flag is set.
+// Converts Osage period 0xf02e to empty character if set.
 // TODO: Convert to UTF-16.
-function oldOsageToUnicode(textIn, convertToLower, convertLatin) {
+function oldOsageToUnicode(textIn, convertToLower, convertLatin, clearOsageDot) {
   var convertResult = "";
   var index;
   var outputIsUTF16 = true;
@@ -242,6 +265,9 @@ function oldOsageToUnicode(textIn, convertToLower, convertLatin) {
   }
   for (index = 0; index < parsedInput.length; index ++) {
     var c = parsedInput[index];
+    if (c == oldOsageDot && clearOsageDot) {
+      continue;
+    }
     var result = osage_private_use_map[c];
     if (result) {
       if (Array.isArray(result)) {
@@ -386,10 +412,8 @@ function latinToOldOsage(textIn, convertToLower) {
 }
 
 var teststring = /abc/;
-var osage_latin_regex = /aa|a\'|ah|an|a|br|b|ch|c|d|ee|en|e|g|hch|hc|hk|hp|hts|ht|hy|h|ii|iu|i|j|ky|k|l|m|n|on|oo|o|p|sh|s|tt|th|tsh|ts|ts\'|t|uu|u|v|w|x|y|zh|z|\'|\[|\]|\\|\/|6|\;|\S|\s/gi;
-var osage_latin_chars = "aa|a\'|ah|an|a|br|b|ch|c|d|ee|en|e|g|hch|hc|hk|hp|hts|ht|hy|h|ii|iu|i|j|ky|k|l|m|n|on|oo|o|p|sh|s|tt|th|tsh|ts|ts\'|t|uu|u|v|w|x|y|zh|z|'|[|]|\\|/|6|\;|,|\\S|\\s";
-
-var combiningDotUpAboveRight = "\u0358";
+var osage_latin_regex = /aa|a\'|ah|an|a^|a|br|b|ch|c|d|ee|en|e^|e|g|hch|hc|hk|hp|hts|ht|hy|h|ii|iu|i|j|ky|k|l|m|n|on|oo|o^|o|p|sh|s|tt|th|tsh|ts|ts\'|t|uu|u^|u|v|w|x|y^|y|zh|z|\'|\[|\]|\\|\/|6|\;|\S|\s/gi;
+var osage_latin_chars = "aa|a\'|ah|an|a\\^|a|br|b|ch|c|d|ee|en|e\\^|e|g|hch|hc|hk|hp|hts|ht|hy|h|ii|iu|i|j|ky|k|l|m|n|on|oo|o\\^|o|p|sh|s|tt|th|tsh|ts|ts\'|t|uu|u\\^|u|v|w|x|y\\^|y|zh|z|'|[|]|\\|/|6|\;|,|\\S|\\s";
 
 // Use regular expression to greedily process input string, producing list of strings
 // to be converted. E.g., 'htathanh' should give {"ht", "a", "th", "n", "h"}
@@ -400,10 +424,13 @@ function preParseLatin(instring) {
 
 // For converting input to sets of connected characters.
 var old_osage_regex = /\uf021|\uf022|\uf023|\uf024|\uf025|\uf026|\uf027|\uf028|\uf029|\uf02a|\uf02b|\uf02c|\uf02d|\uf02e|\uf02f|\uf030|\uf031|\uf032|\uf033|\uf034|\uf035|\uf036|\uf037|\uf038|\uf039|\uf03a|\uf03b|\uf03c|\uf03d|\uf03e|\uf03f|\uf040|\uf041\uf041|\uf041|\uf042|\uf043|\uf044|\uf045\uf045|\uf045|\uf048\uf043|\uf048\uf04b|\uf048\uf050|\uf048\uf044|\uf048\uf05d|\uf048|\uf049|\uf04a|\uf04b|\uf04c|\uf04d|\uf04e|\uf04f\uf04f|\uf04f|\uf050|\uf053|\uf054|\uf055\uf055|\uf055|\uf056|\uf057|\uf058|\uf059\uf059|\uf059|\uf05a|\uf05b|\uf05c|\uf05d|\uf05e|\uf05f|\uf060|\uf061|\uf065|\uf06f|\uf07b|\uf07c|\uf07d|\uf07e|\uf0b6|\S|\s/gi;
-var old_osage_chars = "\uf021|\uf022|\uf023|\uf024|\uf025|\uf026|\uf027|\uf028|\uf029|\uf02a|\uf02b|\uf02c|\uf02d|\uf02e|\uf02f|\uf030|\uf031|\uf032|\uf033|\uf034|\uf035|\uf036|\uf037|\uf038|\uf039|\uf03a|\uf03b|\uf03c|\uf03d|\uf03e|\uf03f|\uf040|\uf041\uf041|\uf041|\uf042|\uf043|\uf044|\uf045\uf045|\uf045|\uf048\uf043|\uf048\uf04b|\uf048\uf050|\uf048\uf044|\uf048\uf05d|\uf048|\uf049|\uf04a|\uf04b|\uf04c|\uf04d|\uf04e|\uf04f\uf04f|\uf04f|\uf050|\uf053|\uf054|\uf055\uf055|\uf055|\uf056|\uf057|\uf058|\uf059\uf059|\uf059|\uf05a|\uf05b|\uf05c|\uf05d|\uf05e|\uf05f|\uf060|\uf061|\uf065|\uf06f|\uf07b|\uf07c|\uf07d|\uf07e|\uf0b6|";
+var old_osage_chars = "\uf041\\^|\uf045\\^|\uf04f\\^|\uf055\\^|\uf059\\^|\uf021|\uf022|\uf023|\uf024|\uf025|\uf026|\uf027|\uf028|\uf029|\uf02a|\uf02b|\uf02c|\uf02d|\uf02e|\uf02f|\uf030|\uf031|\uf032|\uf033|\uf034|\uf035|\uf036|\uf037|\uf038|\uf039|\uf03a|\uf03b|\uf03c|\uf03d|\uf03e|\uf03f|\uf040|\uf041\uf041|\uf041|\uf042|\uf043|\uf044|\uf045\uf045|\uf045|\uf048\uf043|\uf048\uf04b|\uf048\uf050|\uf048\uf044|\uf048\uf05d|\uf048|\uf049|\uf04a|\uf04b|\uf04c|\uf04d|\uf04e|\uf04f\uf04f|\uf04f|\uf050|\uf053|\uf054|\uf055\uf055|\uf055|\uf056|\uf057|\uf058|\uf059\uf059|\uf059|\uf05a|\uf05b|\uf05c|\uf05d|\uf05e|\uf05f|\uf060|\uf061|\uf065|\uf06f|\uf07b|\uf07c|\uf07d|\uf07e|\uf0b6|";
+
+var temp_parse = "a\\^|A\^|e\^|e\^|\uf041\\^|\uf045\\^|\uf04f\\^|\uf055\\^|\uf059\\^";
 
 function preParseOldOsage(instring) {
   var combined_chars = old_osage_chars + osage_latin_chars;
+  //var combined_chars = temp_parse;  // *** TEMPORARY
   var regex2 = new RegExp(combined_chars, "gi");
   var outList = instring.match(regex2);
   //var outList = instring.match(old_osage_regex);
--- js/osage_utils.js
+++ js/osage_utils.js
@@ -1,4 +1,37 @@
 // Fills the input with all the characters in the new Unicode Osage range.
+var unicodeVowels = [0x104b0, 0x104b7, 0x104bb, 0x104c2, 0x104ce,
+  0x104d8, 0x104df, 0x104e3, 0x104ea, 0x104f6];
+
+  // Fills the input with all the characters in the old Osage range.
+  function fillWithOldOsage(target, hex_target) {
+    var output_text = document.getElementById(target);
+    var outputString = "";
+    var codePt;
+    for (codePt = 0xf020; codePt < 0xf046; codePt++) {
+      outputString += String.fromCodePoint(codePt);
+    }
+    outputString += String.fromCodePoint(0x000a);
+
+    for (codePt = 0xf048; codePt < 0xf051; codePt++) {
+      outputString += String.fromCodePoint(codePt);
+    }
+    for (codePt = 0xf053; codePt < 0xf062; codePt++) {
+      outputString += String.fromCodePoint(codePt);
+    }
+    outputString += String.fromCodePoint(0xf065);
+    outputString += String.fromCodePoint(0xf06f);
+    outputString += String.fromCodePoint(0xf07b);
+    outputString += String.fromCodePoint(0xf07c);
+    outputString += String.fromCodePoint(0xf07d);
+    outputString += String.fromCodePoint(0xf07e);
+    outputString += String.fromCodePoint(0xf0b6);
+
+    output_text.innerHTML = outputString;
+    output_text.value = outputString;
+
+    updateHex(target, hex_target);
+  }
+
 function fillWithUnicodeOsage(target, hex_target) {
   var output_text = document.getElementById(target);
   var outputString = "";
@@ -17,9 +50,11 @@ function fillWithUnicodeOsage(target, hex_target) {
   }
 }
 
+// Special cases.
 function fillOsageCombos(target, hex_target) {
   var output_text = document.getElementById(target);
-  outputString = " ";
+  outputString = " \n^^^^^" +
+    "\na^e^o^u^y^A^E^O^U^Y^\naeo";
   output_text.innerHTML = outputString;
   output_text.value = outputString;
   if (hex_target) {
@@ -31,7 +66,7 @@ function fillWithUnicodeOsageMacron(target, hex_target) {
   var output_text = document.getElementById(target);
   var outputString = "";
   var macron = "\u0304";
-  for (var codePt = 0x104b0; codePt <= 0x104d3; codePt++) {
+  for (var codePt in unicodeVowels) {
     outputString += getUnicodeCharacter(codePt);
     outputString += macron;
   }
@@ -68,15 +103,23 @@ function fillWithUnicodeOsageDotted(target, hex_target) {
 function fillWithUnicodeOsageStringAppended(target, stringToAppend, hex_target) {
   var output_text = document.getElementById(target);
   var outputString = "";
-  for (var codePt = 0x104b0; codePt <= 0x104d3; codePt++) {
-    outputString += getUnicodeCharacter(codePt);
+  String.fromCodePoint(0x104b0) + String.fromCodePoint(0x104b7) +
+  String.fromCodePoint(0x104bb) + String.fromCodePoint(0x104c2) +
+  String.fromCodePoint(0x104ce) +
+  String.fromCodePoint(0x104d8) + String.fromCodePoint(0x104df) +
+  String.fromCodePoint(0x104ce3) + String.fromCodePoint(0x104ea) +
+  String.fromCodePoint(0x104f6);
+  for (var index in unicodeVowels) {
+  //for (var codePt = 0x104b0; codePt <= 0x104d3; codePt++) {
+    outputString += getUnicodeCharacter(unicodeVowels[index]); //getUnicodeCharacter(codePt);
     outputString += stringToAppend;
   }
-  outputString += "\n";
+/*  outputString += "\n";
   for (var codePt = 0x104d8; codePt <= 0x104fb; codePt++) {
     outputString += getUnicodeCharacter(codePt);
     outputString += stringToAppend;
   }
+  */
   output_text.innerHTML = outputString;
   output_text.value = outputString;
 
@@ -112,6 +155,7 @@ function fillWithLatinOsage(target, hex_target, modifier) {
   }
   output_text.innerHTML = outputString;
   output_text.value = outputString;
+   updateHex(target, hex_target);
 }
 
 // Check text encoding.  If in Zawgyi, return converted text in Unicode.
@@ -123,6 +167,25 @@ function convertLatinToOldOsage(oldIn, newOut) {
     // Get flag indicating lower case conversion
     var convertToLower = document.getElementById("DoLower2").checked;
     var uText = latinToOldOsage(input_text.value, convertToLower);
+    output_text.innerHTML = uText;
+    output_text.value = uText;
+
+    updateHex(newOut, hex_output_id);
+  }
+
+  // Check text encoding.
+  function convertToUnicode(oldIn, newOut) {
+    var input_text = document.getElementById(oldIn);
+    var output_text = document.getElementById(newOut);
+    var hex_output_id = "new_hex";
+
+    // Call Unicode converter.
+    // Get flags.
+    var convertToLower = document.getElementById("DoLower").checked;
+    var convertLatin = document.getElementById("convertLatin").checked;
+    var clearOsageDot = document.getElementById("clearOsageDot").checked;
+    var uText = oldOsageToUnicode(input_text.value,
+      convertToLower, convertLatin, clearOsageDot);
     output_text.innerHTML = uText;
     output_text.value = uText;
 
