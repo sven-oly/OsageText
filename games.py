21898d5738404ac78823751a78cc6b427d8e2188
Craig Cornelius <craig@craigs-mac-mini.attlocal.net>
2018-02-16 07:37:29 -0800

    Starting to create web page for word search geerator.

--- games.py
+++ games.py
@@ -4,6 +4,8 @@
 
 import main
 
+import wordsearch
+
 from userDB import getUserInfo
 
 import blobstorage
@@ -33,7 +35,23 @@ class WordSearchHandler(webapp2.RequestHandler):
     path = os.path.join(os.path.dirname(__file__), 'wordsearch.html')
     self.response.out.write(template.render(path, template_values))
 
+
+class GenerateWordSearchHandler(webapp2.RequestHandler):
+  def get(self):
+    user_info = getUserInfo(self.request.url)
+    user = users.get_current_user()
+
+    template_values = {
+      'user_nickname': user_info[1],
+      'user_logout': user_info[2],
+      'user_login_url': user_info[3],
+      'language': main.Language,
+    }
+    path = os.path.join(os.path.dirname(__file__), 'wordsearch.html')
+    self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([
     ('/games/wordsearch/', WordSearchHandler),
-
+    ('/games/generatewordsearch/', GenerateWordSearchHandler),
 ], debug=True)
